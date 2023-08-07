437f3ea51caaee005fd8c5ff5aab2536670df636
emochka2007 <nikitapoznyakov@gmail.com>
2023-08-07 15:24:40 +0300

    finished chatbot style

--- pages/chatbot-list/index.tsx
+++ pages/chatbot-list/index.tsx
@@ -4,20 +4,6 @@ import Head from 'next/head';
 const ChatbotListPage: FC = () => {
   return (
     <>
-      <Head>
-        <script
-          src="http://localhost:5050/static/scripts/embed-script.js"
-          defer
-        ></script>
-      </Head>
-      <div>
-        <iframe
-          src="http://localhost:3000/chatbot-iframe/64d0d27d147e3c224f1a9528"
-          width="100%"
-          style={{ height: '100%', minHeight: '700px' }}
-          id="godman-chatbot"
-        ></iframe>
-      </div>
       <ChatbotsList />
     </>
   );
