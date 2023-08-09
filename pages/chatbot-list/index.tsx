ae52ae2a2be6ac174ae5c1aa8d5ac5bccf1599e5
emochka2007 <nikitapoznyakov@gmail.com>
2023-08-09 15:10:55 +0300

    design+delete

--- pages/chatbot-list/index.tsx
+++ pages/chatbot-list/index.tsx
@@ -4,15 +4,15 @@ import Head from 'next/head';
 const ChatbotListPage: FC = () => {
   return (
     <>
-      <iframe
-        src="http://localhost:3000/chatbot-iframe/64cd0ca018e6d0abf364f209"
-        width="100%"
-        id="godman-chatbot"
-      ></iframe>
-      <script
-        src="http://localhost:5050/static/scripts/embed-script.js"
-        defer
-      ></script>
+      {/*<iframe*/}
+      {/*  src="http://localhost:3000/chatbot-iframe/64cd0ca018e6d0abf364f209"*/}
+      {/*  width="100%"*/}
+      {/*  id="godman-chatbot"*/}
+      {/*></iframe>*/}
+      {/*<script*/}
+      {/*  src="http://localhost:5050/static/scripts/embed-script.js"*/}
+      {/*  defer*/}
+      {/*></script>*/}
       <ChatbotsList />
     </>
   );
