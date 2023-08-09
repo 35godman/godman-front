ff8de209346b7b3c2f68001e8f752dbc9354258d
emochka2007 <nikitapoznyakov@gmail.com>
2023-08-09 21:19:48 +0300

    added test config

--- src/helpers/obj/__tests__/convertMessagesToArray.test.js
+++ src/helpers/obj/__tests__/convertMessagesToArray.test.js
@@ -1,21 +1,21 @@
-// import { fireEvent } from '@testing-library/react';
-import { convertMessagesToArray } from '../convertMessagesToArray';
-
-// const Button = ({ text, onClick }) => (
-//   <button testID="button-test" onClick={onClick}>
-//     {text}
-//   </button>
-// );
-
-test('convertMessagesToArray', () => {
-  // const testFunc = jest.fn();
-  // const testComponent = renderer
-  //   .create(<Button text="Test" onClick={testFunc} />)
-  //   .toJSON();
-
-  // const but = testComponent.getByTestId('button-test');
-  // fireEvent.click(but);
-
-  expect(convertMessagesToArray('hello\nworld').length).toEqual(2);
-  // expect(testFunc).toBeCalled();
-});
+// // import { fireEvent } from '@testing-library/react';
+// import { convertMessagesToArray } from '../convertMessagesToArray';
+//
+// // const Button = ({ text, onClick }) => (
+// //   <button testID="button-test" onClick={onClick}>
+// //     {text}
+// //   </button>
+// // );
+//
+// test('convertMessagesToArray', () => {
+//   // const testFunc = jest.fn();
+//   // const testComponent = renderer
+//   //   .create(<Button text="Test" onClick={testFunc} />)
+//   //   .toJSON();
+//
+//   // const but = testComponent.getByTestId('button-test');
+//   // fireEvent.click(but);
+//
+//   expect(convertMessagesToArray('hello\nworld').length).toEqual(2);
+//   // expect(testFunc).toBeCalled();
+// });
