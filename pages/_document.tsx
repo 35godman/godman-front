7dd1e2def6db9560d32b0835df10e996da326833
emochka2007 <nikitapoznyakov@gmail.com>
2023-08-10 12:12:58 +0300

    design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
-console.log('remove script');
 class MyDocument extends Document {
   render() {
     return (
