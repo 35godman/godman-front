a26dbebbb7bf092586cad5d6fa63788fec49493c
RomanYakovlev94 <80695880+RomanYakovlev94@users.noreply.github.com>
2023-09-22 16:07:06 +0300

    updt chat

--- src/entities/UI/InputAskAI/InputAskAI.tsx
+++ src/entities/UI/InputAskAI/InputAskAI.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Button } from 'antd';
 import s from './InputAskAI.module.css';
+import cn from 'classnames';
+import { AnswerComp } from './AnswerComp';
+import { AskComp } from './AskComp';
 
 export const InputAskAI = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [showChat, setShowChat] = useState(false);
+
+  const handleInputChange = (e: any) => {
+    setInputValue(e.target.value);
+    if (e.target.value !== '') {
+      setShowChat(true);
+    } else {
+      setShowChat(false);
+    }
+  };
+
   return (
-    <div className={s.wrapper}>
-      <Input className={s.input} placeholder="Ask the AI about Godman" />
-      <Button className={s.btn}>Enter</Button>
+    <div className={showChat ? cn(s.chat, s.open) : s.chat}>
+      {showChat && (
+        <div className={s.messagesWrapper}>
+          <AnswerComp text="What is the Godman.AI?" />
+          <AskComp text="Godman.AI is a n IDE (Integrated Development Enviroment) that is designed for constructing your own chatbot in collaboration with a powerfull AI " />
+          <AnswerComp text="What is the Godman.AI?" />
+          <AskComp text="Godman.AI is a n IDE (Integrated Development Enviroment) that is designed for constructing your own chatbot in collaboration with a powerfull AI " />
+        </div>
+      )}
+      <div className={s.wrapper}>
+        <Input
+          className={s.input}
+          placeholder="Ask the AI about Godman"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Button className={s.btn}>Enter</Button>
+      </div>
     </div>
   );
 };
--- src/entities/UI/LogIn/LogIn.tsx
+++ src/entities/UI/LogIn/LogIn.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './LogIn.module.css';
 import { Input } from 'antd';
 import { BtnUniv } from '../Buttons/Buttons';
+import Link from 'next/link';
 
 export const LogIn = () => {
   return (
@@ -16,15 +17,45 @@ export const LogIn = () => {
           <p className={s.logoText}>Godman.AI</p>
         </div>
         <p className={s.text}>Log in to Godman.AI to continue</p>
+        <img
+          className={s.imgBorder}
+          src={'imgGeneralPage/border_line.png'}
+          alt=""
+        />
       </div>
-      <div>
+      <div className={s.bodyWrapper}>
         <Input className={s.input} placeholder="Enter your business email" />
         <BtnUniv type="primary" text="Continue" />
         <p className={s.logInAsk}>
-          Don’t have an account? <span className={s.span}>Sign Up Free</span>
+          Don’t have an account?{' '}
+          <Link href={'/'} className={s.spanLink}>
+            Sign Up Free
+          </Link>
         </p>
       </div>
-      <div></div>
+      <div className={s.continue}>
+        <img
+          className={s.imgContinue}
+          src={'imgGeneralPage/continue_line.png'}
+          alt=""
+        />
+        <p className={s.contText}>or continue with</p>
+        <img
+          className={s.imgContinue}
+          src={'imgGeneralPage/continue_line.png'}
+          alt=""
+        />
+      </div>
+      <div className={s.footer}>
+        By log in you agree to{' '}
+        <Link className={s.linkFooter} href="/">
+          Terms of Use
+        </Link>{' '}
+        and{' '}
+        <Link className={s.linkFooter} href="/">
+          Privacy Policy
+        </Link>
+      </div>
     </div>
   );
 };
